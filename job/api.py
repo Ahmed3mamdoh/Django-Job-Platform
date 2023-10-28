4efa141a776bd0b6d46d82f52839f5c2bb70839c
Ahmed3mamdoh <ahmed3mmdh@gmail.com>
2023-10-29 00:50:48 +0200

    add Create , Update , Delete to API List & API Detail

--- job/api.py
+++ job/api.py
@@ -1,18 +1,33 @@
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
+from rest_framework import generics
 from .serializers import JobSerializers
 from .models import Job
 
 
-@api_view(['GET'])
-def job_list_api(request):
-    jobs = Job.objects.all()
-    data = JobSerializers(jobs,many=True).data
-    return Response({'Jobs':data})
+#@api_view(['GET'])
+#def job_list_api(request):
+    #jobs = Job.objects.all()
+    #data = JobSerializers(jobs,many=True).data
+    #return Response({'Jobs':data})
 
 
-@api_view(['GET'])
-def job_detail_api(request,id):
-    job = Job.objects.get(id=id)
-    data = JobSerializers(job).data
-    return Response({'Job':data})
+#@api_view(['GET'])
+#def job_detail_api(request,id):
+    #job = Job.objects.get(id=id)
+    #data = JobSerializers(job).data
+    #return Response({'Job':data})
+
+#-------------------CBV---------------------
+
+
+
+class JobListAPI(generics.ListCreateAPIView):
+    queryset = Job.objects.all()
+    serializer_class = JobSerializers
+
+
+
+class JobDetailAPI(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Job.objects.all()
+    serializer_class = JobSerializers
--- job/urls.py
+++ job/urls.py
@@ -1,11 +1,11 @@
 from .views import job_list , job_details
 from django.urls import path
-from .api import job_list_api , job_detail_api
+from .api import JobListAPI , JobDetailAPI
 
 
 urlpatterns = [
     path('', job_list),
     path('<slug:slug>', job_details),
-    path('api/list' , job_list_api),
-    path('api/list/<int:id>' , job_detail_api),
+    path('api/list' , JobListAPI.as_view()),
+    path('api/list/<int:pk>' , JobDetailAPI.as_view()),
 ]
